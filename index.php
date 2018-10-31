0e0ac73c32dce035e058a42a12f7db1190d8e52a
Chatura Ahangama <cahangama@csumb.edu>
2018-10-31 06:01:25 +0000

    update

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ echo 'Chaturas Web Page!';
 echo  '<ul>
         <h3>Labs</h3>
         <li><a href="./Labs/Lab1/index.html"><strong>Lab 1</strong></a></li>
+        <li><a href="./Labs/Lab2/index.php"><strong>Lab 2</strong></a></li>
+         <h3>HomeWork</h3>
         <li><a href="./HW/HW1/index.html"><strong>Hw 1</strong></a></li>
        </ul>'
 ?>
