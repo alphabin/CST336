76dbf3f3d68e3bcd859db2d18a8535c6a410d4c1
Chatura Ahangama <cahangama@csumb.edu>
2018-11-11 06:22:05 +0000

    update w the latest stable ssh

--- HW/HW3/clientPHP.php
+++ HW/HW3/clientPHP.php
@@ -1,8 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SSH Command Executor</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Bangers|Kumar+One+Outline" rel="stylesheet">
+        <style type="text/css">
+            @import url("css/styles.css");
+        </style>
+        
+    </head>
+    <body>
+
+
 <?php
 include('Net/SSH2.php');
 include('File/ANSI.php');
+define('NET_SSH2_LOGGING', NET_SSH2_LOG_COMPLEX);
 
 
+/*Helper Class for the PHP SSH session */
 class PHPSSHWrapper {
     
     private $sshConnection;
@@ -23,16 +39,12 @@ class PHPSSHWrapper {
               return true;
         }
     }
-    
-    public function Debugging($option=false)
+   
+   public function Debugging($option="false")
     {
-        if($option == true)
+        if($option == "true")
         {
-         define('NET_SSH2_LOGGING', NET_SSH2_LOG_COMPLEX);
-        }
-        else
-        {
-            error_reporting(E_ERROR | E_PARSE);
+           echo $this->sshConnection->getLog();
         }
     }
     
@@ -61,8 +73,72 @@ class PHPSSHWrapper {
     
 }
 
-$myTerm = new PHPSSHWrapper($_GET['ip']);
-$myTerm->Debugging(false);
-$myTerm->Connect($_GET['user'],$_GET['password']);
-    echo $myTerm->RunCmd("cd ~ | ls -la");
+$myTerm =null;
+
+
+
+
+
+if(!isset($_GET['ip']))
+{
+     echo "<div class=\"errorMessage\"> Please submit a Request! </div>";
+     exit();
+}
+else
+{
+    if (filter_var($_GET['ip'], FILTER_VALIDATE_IP)) {
+        $myTerm = new PHPSSHWrapper($_GET['ip']);
+} else {
+        echo "<div class=\"errorMessage\"> Please Enter a valid IP address for the Request! </div>";
+        exit();
+}
+        
+}
+
+
+if(isset($_GET['user']) && isset($_GET['password']))
+{
+    if(strlen($_GET['user']) > 0 && strlen($_GET['password']) > 0)
+    {
+    $myTerm->Connect($_GET['user'],$_GET['password']);
+    
+        if($_GET['runPreCommand'] == "true")
+        {
+            if($_GET['cannedCommand'] != "" )
+             echo $myTerm->RunCmd($_GET['cannedCommand']);
+            else
+             echo "<div class=\"errorMessage\">You didnt select a correct Cannned command for linux </div>";
+        }
+        else
+        {
+            if($_GET['command'] != "" )
+                echo $myTerm->RunCmd($_GET['command']);
+            else
+             echo "<div class=\"errorMessage\">You didnt type a command </div>";
+        }
+        if(isset($_GET['debug']))
+          {
+           if( $_GET['debug'] == "true")
+             echo $myTerm->Debugging("true");
+          }
+    }
+    else{
+        echo "<div class=\"errorMessage\">Login User/Password Information Invalid </div>";
+        exit();
+    }
+}
+else{
+     echo "<div class=\"errorMessage\">Login Information Invalid </div>";
+        exit();
+}
+
 ?>
+
+
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ HW/HW3/index.php
@@ -0,0 +1,97 @@
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>SSH Command Executor</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Bangers|Kumar+One+Outline" rel="stylesheet">
+    
+        <style type="text/css">
+            @import url("css/styles.css");
+        </style>
+        
+    </head>
+    <body>
+       
+        <div class="jumbotron jumbotron-fluid vertical-center">
+            <h1 id="headerJumbo" class="display-4">Welcome to the SSH Terminal Command Center</h1>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6  col-sm-12 center">
+            <form method="get" target="my-iframe" action="clientPHP.php">
+              <table >
+                    <tr>
+                        <th>IP Address</th>
+                        <td>
+                            <input id="ipAddr"class="form-control" type="text" name="ip">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>UserName
+                        </th>
+                        <th>Password
+                        </th> 
+                    </tr>
+                    <tr>
+                        <td>
+                            <input class="form-control" type="text" name="user">
+                        </td>
+                        <td>
+                            <input  class="form-control" type="password" name="password">
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Command To Execute</th>
+                        <td>
+                        <textarea id="textCLI" class="form-control" name="command" rows="2" cols="20"></textarea>
+                        </td>
+                    </tr>
+                    <tr>
+                         <td></td>
+                         <td>
+                             <input class="form-check-input" type="checkbox" name="runPreCommand" value="true" >Run a canned Linux Command<br>
+                             <select id="selectForm" class="form-control" name="cannedCommand">
+                                  <option value="">Chose One</option> 
+                                  <option value="ls -la">List Directory</option>
+                                  <option value="whoami">Check UserName</option>
+                                  <option value="uptime">Check Uptime</option>
+                                  <option value="netstat -lntu">Check OpenPorts</option>
+                                  <option value="df -h">Check Files Space</option>
+                                  <option value="mount">Check Mount</option>
+                            </select>
+                        </td>
+                    </tr>
+                   
+                    <tr>
+                        <td>
+                            <input type="radio" name="debug" value="false" checked>Debug OFF<br>
+                            <input type="radio" name="debug" value="true">Debug ON
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                              <input class="btn btn-primary " type="submit" value="Submit">
+                        </td>
+                        <td>
+                              <input id="resetButton" class="btn btn-primary red" type="reset">
+                        </td>
+                    
+                    </tr>
+             </table>
+            </form>
+                </div>
+                <div class="col-lg-6  col-sm-12">
+                    <iframe name="my-iframe" width="100%" height="100%" src="clientPHP.php"></iframe>
+                </div>
+            </div>
+           
+        </div>
+        </div>
+        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+        </body>
+</html>
